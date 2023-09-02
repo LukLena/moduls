b27ac205aeff9ca01c1e766f79135ba3aac46c2e
LukLena <106184354+LukLena@users.noreply.github.com>
2023-09-02 15:08:15 +0300

    add game

---
+++ src/game.js
@@ -0,0 +1,16 @@
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  import Character from './domain.js';
+  console.log(Character)
+  
+  export class GameSavingData {
+  }
+  
+  export function readGameSaving() {
+  }
+  
+  export function writeGameSaving() {
+  }
